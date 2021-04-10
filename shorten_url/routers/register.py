036cd30458c6f0c56b78aa1589cb1ce356521a40
sam5727 <x6513750@gmail.com>
2021-04-11 06:33:38 +0800

    Add clear response while POSTing

--- shorten_url/routers/register.py
+++ shorten_url/routers/register.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 from .. import schemas, database, models
 
@@ -7,8 +7,8 @@ router = APIRouter(
     tags=['Register']
 )
 
-@router.post('/')
-def get_shorten_url(request: schemas.Url, db: Session = Depends(database.get_db)):
+@router.post('/', status_code=status.HTTP_200_OK)
+def get_shorten_url(request: schemas.Url, response: Response, db: Session = Depends(database.get_db)):
     new_shorten = db.query(models.Url).filter(models.Url.original_url == request.original_url).first()
     if not new_shorten:
         shorten_url = request.original_url[0:4]
@@ -16,4 +16,5 @@ def get_shorten_url(request: schemas.Url, db: Session = Depends(database.get_db)
         db.add(new_shorten)
         db.commit()
         db.refresh(new_shorten)
+        response.status_code = status.HTTP_201_CREATED
     return new_shorten
